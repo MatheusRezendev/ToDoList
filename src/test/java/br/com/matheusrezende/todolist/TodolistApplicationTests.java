6862b774e189634adcde158fe0cd21d154b2be94
Matheus Rezende <contatorzn@hotmail.com>
2023-10-18 20:45:14 -0300

    todolist

---
+++ src/test/java/br/com/matheusrezende/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheusrezende.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
